2d796441fff87f3596a0318ccce913aa889d7726
zhaojia <905518706@qq.com>
2018-08-03 09:51:47 +0800

    1

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,13 @@ Route::get('/', function () {
 
 Route::any('index','Admin\AdminController@index');
 //世明影片管理列表
-Route::any('movie_list','Admin\AdminController@movie_list');
+Route::any('movie_list','Admin\MovieController@movie_list');
 //黄笑放映厅管理列表
-Route::any('movie_house_list','Admin\AdminController@movie_house_list');
+Route::any('movie_house_list','Admin\HouseController@movie_house_list');
 //世超放映厅安排列表
-Route::any('movie_plan_list','Admin\AdminController@movie_plan_list');
+Route::any('movie_plan_list','Admin\PlanController@movie_plan_list');
+
+
 //赵佳会员列表
 Route::any('vip_list','Admin\VipController@vip_list');
 /*
