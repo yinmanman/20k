4bc6b419011f611894d999e221626cc47234a90e
zsm123456123456 <982008656@qq.com>
2018-08-08 11:36:30 +0800

    0.0

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,24 @@ Route::get('/', function () {
 });
 
 Route::any('index','Admin\AdminController@index');
+
+
 //世明影片管理列表
 Route::any('movie_list','Admin\MovieController@movie_list');
+//世明影片管理列表_增
+Route::any('addMovie','Admin\MovieController@addMovie');
+//世明影片管理列表_删
+Route::any('delMovie','Admin\MovieController@delMovie');
+//世明影片管理列表_改
+Route::any('updateMovie','Admin\MovieController@updateMovie');
+//世明影片管理列表_增页面
+Route::any('movieAdd','Admin\MovieController@movieAdd');
+//世明影片管理列表_删页面
+Route::any('movieDel','Admin\MovieController@movieDel');
+//世明影片管理列表_改页面
+Route::any('movieUpdate','Admin\MovieController@movieUpdate');
+
+
 //黄笑放映厅管理列表
 Route::any('movie_house_list','Admin\HouseController@movie_house_list');
 //世超放映厅安排列表
